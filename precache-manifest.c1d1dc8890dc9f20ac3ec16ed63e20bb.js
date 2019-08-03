d9ded6f3eb297bc1edcbd95e70301de96aa4d38e
oscar <oscar@o2webdev.com>
2019-08-03 12:25:40 -0400

    Updates

---
+++ precache-manifest.c1d1dc8890dc9f20ac3ec16ed63e20bb.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32ba5c2d01cf67468e1e55b6f81d3fd2",
+    "url": "/hello-hooks/index.html"
+  },
+  {
+    "revision": "3e5ec0f76c0f7140f1fa",
+    "url": "/hello-hooks/static/js/2.28d6930a.chunk.js"
+  },
+  {
+    "revision": "9a0944927c2a501c4e07",
+    "url": "/hello-hooks/static/js/main.dbc58f96.chunk.js"
+  },
+  {
+    "revision": "dd3d945418783037992c",
+    "url": "/hello-hooks/static/js/runtime~main.1f21b8ce.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/hello-hooks/precache-manifest.34e7931bd7792cfa62e8a85b8c6dabc3.js"
+  "/hello-hooks/precache-manifest.c1d1dc8890dc9f20ac3ec16ed63e20bb.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/hello-hooks/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/hello-hooks/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
